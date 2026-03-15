edd0d3bf4057537b70f6de206a6c09c6b36fb5b8
Juan Martín Maidana <martinmai98@gmail.com>
2026-03-15 11:23:35 -0300

    Creación de tabla de usuarios vacía

---
+++ migraciones/003_add_tabla_usuarios.sql
@@ -0,0 +1,12 @@
+-- Creamos la tabla de usuarios
+CREATE TABLE IF NOT EXISTS aida.usuarios (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Le damos permisos al usuario de la aplicación para poder leer y escribir (por si acaso no usamos el owner)
+GRANT SELECT, INSERT, UPDATE, DELETE ON aida.usuarios TO aida_admin;
+GRANT USAGE, SELECT ON SEQUENCE aida.usuarios_id_seq TO aida_admin;
