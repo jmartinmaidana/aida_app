65735bba0716add7c034b01fe85837637b4fcaf8
Juan Martín Maidana <martinmai98@gmail.com>
2026-05-01 20:40:18 -0300

    Fix port

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -8,7 +8,7 @@ export class EmailService {
     private static transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST || 'smtp.gmail.com',
         port: Number(process.env.EMAIL_PORT) || 587,
-        secure: false, // true para el puerto 465, false para los demás
+        secure: Number(process.env.EMAIL_PORT) === 465, // Automáticamente true si usamos el puerto 465
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
